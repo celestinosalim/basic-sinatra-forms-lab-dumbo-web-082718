59845a548090a7b2982b08761ae083f0b28a208b
celestinosalim <celestino.salim@gmail.com>
2018-09-18 23:23:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,21 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
 
+
+get '/newteam' do
+  erb :newteam
+end
+
+post '/newteam' do
+  @teamname = params[:name]
+  @coach = params[:coach]
+  @pg = params[:pg]
+  @pf = params[:pf]
+  @sg = params[:sg]
+  @sf = params[:sf]
+  @c = params[:c]
+  erb :team
+end
+
+
 end
